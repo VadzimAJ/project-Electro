884104a8ba2c9547cacbbb2a09c060ba9e7eb6a8
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-08-18 16:32:56 +0300

    feat: cerate v_btranch

--- electro-project/src/App.tsx
+++ electro-project/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Electro Project V
+          Vadim Project
         </p>
         <a
           className="App-link"
