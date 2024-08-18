37f804f52d3a58ea5f632ac798251fac400f8ba9
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-08-18 17:10:11 +0300

    feat: create header bar and products featls

--- electro-project/src/App.tsx
+++ electro-project/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HeaderBar } from './HeaderBar';
+import { StoreBar } from './StoreBar';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vadim Project
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderBar/>
+      <StoreBar/>
     </div>
+    
   );
 }
 
