f8b184cf581218623b398e479f94cb6cd3675a61
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2024-08-19 19:54:32 +0300

    feat: create start demo list of goods

--- electro-project/src/App.tsx
+++ electro-project/src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HeaderBar } from './HeaderBar';
 import { StoreBar } from './StoreBar';
 
-
+export type ElectroProductsType = {
+  name: string
+  cost: number
+  count: number
+}
 
 
 function App() {
+
+  const [electroProducts, setElectroProducts] = useState( [
+    {name: 'Lamp', cost: 123, count: 0},
+    {name: 'Pachcord', cost: 400, count: 0},
+    {name: 'Transistor', cost: 300, count: 0},
+  ])
   return (
     <div className="App">
       <HeaderBar/>
-      <StoreBar/>
+      <StoreBar 
+        electroProducts = {electroProducts}
+      />
     </div>
     
   );
--- electro-project/src/StoreBar.tsx
+++ electro-project/src/StoreBar.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { ElectroProductsType } from './App'
 
-export const StoreBar = () => {
-  return (
-    <div>StoreBar</div>
+
+type StoreBar = {
+  electroProducts: ElectroProductsType[]
+}
+export const StoreBar = (props:StoreBar) => {
+  return (<div >
+    {props.electroProducts.map((product, index) => (
+      <div  className='produsct-div' key={index}>
+        <span>{product.name}</span>
+        <span>{product.cost}</span>
+        <span>{product.count}</span>
+      </div>
+    ))}
+  </div>
   )
+
+  
 }
